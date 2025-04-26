8e68bc6b1de765f78b0b20258b1415d03c4d72bd
AdityaJrsu17 <71754876+adityajsru@users.noreply.github.com>
2025-04-26 13:41:53 +0530

    Update Physics.js

--- commands/Physics.js
+++ commands/Physics.js
@@ -4,6 +4,10 @@
   need_reply: false
   auto_retry_time: 0
   answer: Choose what you want in Physics:
-  keyboard: Books, Lectures, Modules, Guidance, Doubt Solving, Motivation, Memes, Wiki, Others
+  keyboard: [
+    ["Books", "Lectures", "Modules"],
+    ["Guidance", "Doubt Solving", "Motivation"],
+    ["Memes", "Wiki", "Others"]
+  ]
   aliases: 
 CMD*/
